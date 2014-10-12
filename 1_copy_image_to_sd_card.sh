09d55e410998a3e433fb2f67875d33aa1d6ce57f
moul <ma.azimi@laposte.net>
2014-10-12 20:56:58 +0200

    enh

--- 1_copy_image_to_sd_card.sh
+++ 1_copy_image_to_sd_card.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 # Download Cubian images on http://cubian.org/
-
-# If Cubieboard 1 comment first line and uncomment second one
-CUBIAN_IMG_PATH=./Cubian-nano+headless-x1-a20.img
-#CUBIAN_IMG_PATH=./Cubian-nano+headless-x1-a10.img
-#Check SD card path
-SD_DRIVE=/dev/sdd
+# Cubian-nano+headless-x1-a20.img
+# Cubian-nano+headless-x1-a10.img
 
 # Copy cubian image on SD card
-mount SD_DRIVE
-sudo dd bs=1M if=CUBIAN_IMG_PATH of=SD_DRIVE; sync
-umount SD_DRIVE
+mount /dev/sdd
+sudo dd bs=1M if=Cubian-nano+headless-x1-a20.img of=/dev/sdd; sync
+umount /dev/sdd
 
--- 2_configure_image_and_install_yunohost.sh
+++ 2_configure_image_and_install_yunohost.sh
@@ -6,26 +6,44 @@ nmap box/24
 # SSH connection | password = "cubie"
 ssh -p 36000 cubie@IP
 
-# Get root | password = "cubie"
+# Become root | password = "cubie"
 sudo -i
-
-# Change hostname
-hostname -v yunohost
 
 # Delete Ajenti and his repo
 apt-get remove ajenti
 nano /etc/apt/sources.list
 
+# Upgrade Debian
+apt-get update && apt-get dist-upgrade && apt-get autoremove
+
+# Upgrade Cubian
+apt-get install cubian-update && cubian-update
+
+# Change hostname
+hostname -v YunoHost
+
+# Change rood password to "yunohost"
+passwd root
+
 # Install YunoHost
 git clone https://github.com/YunoHost/install_script /tmp/install_script
 cd /tmp/install_script && sudo ./install_yunohostv2
-# Don’t do postinstallation
+# Do not proceed to postinstallation
+
+# Move port 3600 to 22 by commenting line "Port 36000"
+# Permit ssh connection with root by switching: PermitRootLogin to "yes"
+nano /etc/ssh/sshd_config
+
+# Quit root and SSH connection
+exit && exit
+
+# Try SSH connection with root
+ssh root@IP
 
 # Delete SSH keys
 rm -f /etc/ssh/ssh_host_*
 
 # Add script for regenerate SSH key at boot
-
 nano /etc/init.d/ssh_gen_host_keys
 ---
 #!/bin/sh
@@ -44,27 +62,11 @@ insserv -r /etc/init.d/ssh_gen_host_keys
 rm -f \$0
 ---
 
+# Make it executable
 chmod a+x /etc/init.d/ssh_gen_host_keys
+
+# Make it execute at next boot
 insserv /etc/init.d/ssh_gen_host_keys
-
-# Upgrade Debian
-apt-get update && apt-get dist-upgrade
-
-# Upgrade Cubian
-apt-get install cubian-update && cubian-update
-
-# Mouve port 3600 to 22 by commenting line "Port 36000"
-# Permit ssh connection with root by switching: PermitRootLogin to "yes"
-nano /etc/ssh/sshd_config
-
-# Change rood password to "yunohost"
-passwd root
-
-# Quit SSH connection
-exit
-
-# Try SSH connection with root
-ssh root@IP
 
 # Delete cubie user
 userdel cubie
